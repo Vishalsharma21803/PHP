95ce707d9a62da166135dad0fc9e77da9c41fc25
Vishal Sharma <vishal2003ggg@gmail.com>
2024-09-06 15:36:37 +0530

    new files

--- tute4.php
+++ tute4.php
@@ -16,6 +16,9 @@ $arr=array("Ram","shyam","mohan","rohan");
 print_r(array_chunk($arr,2,true)); // array ko 2-2 ke chunks(parts) mein divide kr dega
 $newarr= array("INT220"=>"PHP", "INT221"=>"Laravel","INT222"=>"Node");
 
+// array_slice()
+print_r("array slice: ".array_slice($newarr,1,2));
+
 // array_diff()
 //array_flip() : interchange keys and values with each other
 $res=array_flip($newarr);
@@ -23,7 +26,11 @@ print_r($res);
 
 // array_intersect(): gives the common elements from all the arrays
 // array_merge(): merges two or more arrays into one array
-
+$course=array("eng2","eng2","mat2","sat2","eng2","pat2");
+print_r(array_merge($arr,$course));
+//array_pop():removes the last element of the array
+//array_reverse(): reverse the array
+//array_search():
 
 
 ?>

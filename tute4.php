275e84b72178b6b086a86217aea68f0d931686c7
Vishal Sharma <vishal2003ggg@gmail.com>
2024-09-06 14:40:40 +0530

    new files

---
+++ tute4.php
@@ -0,0 +1,19 @@
+<?php
+// static variable : remembers its previous value
+function static_var(){
+    static $a=2;
+    $b=2;
+    $a++;
+    $b++;
+    echo"static: $a\n";
+    echo"non-static: $b\n";
+}
+static_var();
+static_var();
+
+$arr=array("Ram","shyam","mohan","rohan");
+// array_chunk()
+print_r(array_chunk($arr,2,true)); // array ko 2-2 ke chunks(parts) mein divide kr dega
+$newarr= array("INT220"=>"PHP", "INT221"=>"Laravel","INT222"=>"Node");
+
+?>
